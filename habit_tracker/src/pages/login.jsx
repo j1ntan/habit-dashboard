4731c48c9c30fbfed004b93648074ea74045c308
nitesh <nitesh6742patel@gmail.com>
2025-01-20 08:40:01 +0530

    created login page

--- habit_tracker/src/pages/login.jsx
+++ habit_tracker/src/pages/login.jsx
@@ -2,7 +2,22 @@ import './login.css'
 
 function Login(){
     return(
-        <h1>Login_Page</h1>
+        <div class="login">    
+            <div class="login2">
+                <div class="login3">
+                    <h2>Login</h2>
+                </div>
+                <div class="noAccount">
+                    <p>Don't have an Account ?</p>
+                    <button><a href="#">Click here</a></button>
+                </div>
+                <h3>E-mail</h3>
+                <input></input>
+                <h3>Password</h3>
+                <input></input>  
+                <button class="submit">Login</button>
+            </div>         
+        </div>
     );
 }
 
