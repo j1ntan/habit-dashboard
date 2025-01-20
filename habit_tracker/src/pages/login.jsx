d82a119282d1980c2b27df18ad201e3de686388f
nitesh <nitesh6742patel@gmail.com>
2025-01-20 11:07:07 +0530

    updated login page with don't have an account

--- habit_tracker/src/pages/login.jsx
+++ habit_tracker/src/pages/login.jsx
@@ -7,11 +7,15 @@ function Login(){
                 <h1>Login</h1>
                 <form action="submit" method="post">
                     <div class="label"><label for="name">Name</label></div>
-                    <input type="text" name="name" placeholder="Your Name"></input>
+                        <input type="text" name="name" placeholder="Your Name"></input>
                     <div class="label"><label for="email">E-mail</label></div>
-                    <input type="email" name="email" placeholder="E-mail"></input>
+                        <input type="email" name="email" placeholder="E-mail"></input>
                     <div class="label"><label for="password">Password</label></div>
-                    <input type="password" name="password" placeholder="Your Password"></input>
+                        <input type="password" name="password" placeholder="Your Password"></input>
+                    <div class="noAcc">
+                        <p class="para">Don't have an Account ? </p> 
+                        <a href="#">Sign Up</a>
+                    </div>
                     <input type="submit" value="submit" />
                 </form>
             </div>
